585af92c41cbb5ce7c542a0c30953d051adbc063
vilievkofata <49074258+vilievkofata@users.noreply.github.com>
2019-04-04 11:01:31 +0300

    Update SquareOfStars.php

--- 04. Първи стъпки в програмирането/FirstStepsInCoding/SquareOfStars.php
+++ 04. Първи стъпки в програмирането/FirstStepsInCoding/SquareOfStars.php
@@ -1,2 +1,18 @@
 <?php
-//TODO
+$length = readline();
+for ($i = 1; $i <= $length; $i++) {
+    if ($i == 1 or $i == $length) {
+        for ($j = 1; $j <= $length; $j++) {
+            echo '*';
+        }
+    } else {
+        for ($j = 1; $j <= $length; $j++) {
+            if ($j == 1 or $j == $length) {
+                echo '*';
+            } else {
+                echo ' ';
+            }
+        }
+    }
+    echo PHP_EOL;
+}
